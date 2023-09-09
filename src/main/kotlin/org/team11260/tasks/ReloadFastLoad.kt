6d73a3220341c6b8ee761c702a8e73d047867681
Matthew Oates <uni.oates@gmail.com>
2023-09-08 22:21:12 -0400

    added helpful errors for reload problems

--- src/main/kotlin/org/team11260/tasks/ReloadFastLoad.kt
+++ src/main/kotlin/org/team11260/tasks/ReloadFastLoad.kt
@@ -4,7 +4,9 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.process.internal.ExecException
 import java.io.ByteArrayOutputStream
+import java.net.ConnectException
 import java.nio.charset.Charset
 
 /**
@@ -17,18 +19,24 @@ abstract class ReloadFastLoad : DefaultTask() {
 
     @TaskAction
     fun execute() {
-        project.exec {
+        try {
             val output = ByteArrayOutputStream()
-            it.standardOutput = output
-            it.commandLine(
-                getAdbExecutable().get(),
-                "shell",
-                "am",
-                "broadcast",
-                "-a",
-                "team11260.RELOAD_FAST_LOAD",
-            )
-            println(output.toString(Charset.defaultCharset()))
+            project.exec {
+                it.standardOutput = output
+                it.commandLine(
+                    getAdbExecutable().get(),
+                    "shell",
+                    "am",
+                    "broadcast",
+                    "-a",
+                    "team11260.RELOAD_FAST_LOAD",
+                )
+            }
+            if(!output.toString(Charset.defaultCharset()).contains("result=1")) {
+                error("Failed to complete fast load reload, ensure FtcRobotController app is running and has fast-load dependency installed.")
+            }
+        } catch (e: ExecException) {
+            error("Failed to connect to robot, ensure ADB connected to robot.")
         }
     }
 }
